ef2fdb5be90ab7b655af6f1655f488267e9a62a2
Matthew Higgins <156959517+mthiggins1s@users.noreply.github.com>
2025-11-15 11:59:39 -0600

    installed express

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const http = require('http');
-const routes = require('./routes');
-console.log(routes.someText);
+const express = require('express');
 
-// tells node to execute the function stored in routes for incoming requests.
-const server = http.createServer(routes.handler);
+// app needs to be defined BEFORE the server call.
+const app = express();
+// accepts an array of request handlers
+app.use((req, res, next) => {
+    console.log('inside the middleware!');
+    next(); // we call 'next' here so the middleware can travel to the next request!
+});
+app.use((req, res, next) => {
+    console.log('inside another middleware!');
+    res.send('<h1>Hello from Express!</h1>'); // allows us to attach a 'body' to the middleware!
+});
 
-server.listen(3000, () => {
-  console.log('🚀 Server is running at http://localhost:3000');
+app.listen(3000, () => {
+  console.log('🚀 Server is RUNNING!');
 });
