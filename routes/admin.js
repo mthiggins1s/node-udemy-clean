1d451a710175ba0400cb979a6ce8ac9efa912d61
Matthew Higgins <mthiggins1s@semo.edu>
2025-11-18 15:07:09 -0600

    added some styling

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router(); // a router thats tied to the other express app
+const rootDir = require('../utilities/path'); // this will inject path from utilities
 
 router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html')); // additional routes besides the root route need to be called first to make it through the middleware
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router();
+const rootDir = require('../utilities/path'); // this will inject path from utilities
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html')); // holds the ABSOLUTE path in our filesystem
+    res.sendFile(path.join(rootDir, 'views', 'shop.html')); // holds the ABSOLUTE path in our filesystem
 });
 
 module.exports = router;
